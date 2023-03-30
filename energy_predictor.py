99831c840fac341ba4f9457102c7b78e3b0267a5
Ryan Anderson <sonorousduck@gmail.com>
2023-03-30 14:15:48 -0600

    Lots of work, built out an LSTM reinforcement learning algo

--- energy_predictor.py
+++ energy_predictor.py
@@ -28,14 +28,16 @@ class LSTM(nn.Module):
     def forward(self, x):
         h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size * 3)).to("cuda")
         c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size * 3)).to("cuda")
-
         ula, (h_out, _) = self.lstm(x, (h_0, c_0))
         h_out = h_out.view(-1, self.hidden_size * 3)
         out = self.fc(h_out)
         out = self.fc1(out)
         out = self.fc2(out)
+        # out = self.fc3(out)
+        # out = self.fc4(out)
 
         return out
+
 
     # def sliding_windows(self, data):
     #     x = []
--- simulation_no_in_session_curtailment.py
+++ simulation_no_in_session_curtailment.py
@@ -7,35 +7,9 @@ import matplotlib.pyplot as plt
 from energy_predictor import LSTM
 import torch
 from sklearn.preprocessing import MinMaxScaler
+from price_agent import *
+from car import Car
 
-class Car:
-    def __init__(self, car_name, max_battery_size, max_charge_rate=100_000):
-        self.max_charge_rate = max_charge_rate
-        self.initial_soc = 0 # watts
-        self.max_battery_size = max_battery_size # watts
-        self.car_name = car_name
-        self.soc = self.initial_soc / self.max_battery_size
-    
-    # This will be linear to begin with. Then replace with typical battery curve for a Tesla or something like that. If I can build out a better
-    # model, this will be done instead. Currently linear implementation. This will also be replaced
-    def charge(self, charge_amount):
-        self.soc += charge_amount * 0.9 # Assume 90% efficiency of charge rate. Varied amounts due to temperature and currents. Safe to just go with an average of the efficiencies that I saw
-    
-    def set_max_battery_size(self, max_battery_size):
-        self.max_battery_size = max_battery_size
-    
-    def set_initial_charge_percentage(self, current_soc):
-        self.initial_soc = current_soc * max_battery_size
-        self.soc = current_soc * max_battery_size
-    
-    def reset_charge(self):
-        self.soc = self.initial_soc
-
-    # Taking the battery curves, predict when the battery will finish charging. This will be replaced by a prediction algorithm as well.
-    def predict_charging_time_completion(self, charging_rate):
-        approx_time = self.max_battery_size / (charging_rate * 0.9)
-        return approx_time
-  
 
 
 # This will be the thing that generates the "true" energy usage. Will eventually be based on historical data instead. (Get the past 4 days at the leviton)
@@ -196,9 +170,15 @@ if __name__ == "__main__":
     power_usage = [np.array([i[1]]) for i in true_energy_usage]
     sc = MinMaxScaler()
     power_usage = sc.fit_transform(power_usage)
-    lstm = LSTM(1, 1, 1, 1, 24)
+
+    seq_length = 64
+    lstm = LSTM(3, 3, 16, 1, seq_length)
     lstm.load_state_dict(torch.load('models/energy_usage_predictor.pth'))
     lstm.to('cuda')
+
+    actor = Actor(seq_length, 1)
+    critic = Critic(seq_length, 1)
+    
     
     # This simulation will "spawn" a car, and it is our job to charge it. We will have a basic probability distribution that will generate a power consumption for the next hour or so
     # Based upon this generation, and the chance of us knowing what soc they are at and what car they have (i.e. battery size), we will then set a curtailment to charge them as quick as possible but without
@@ -232,6 +212,10 @@ if __name__ == "__main__":
         charge_rate = determine_optimal_charging_rate(myCar, predicted_energy_usage, peak_consumption) # Since it is every 10 minutes, and it is 10_000 watts per hour, you need to do 1/6 of the amount
         charge_rates.append(charge_rate)
         # Now that you got your charge_rate, you then actually charge the car. However, this SOC may be different than the one that you tried determining it on.
+
+        # Reinforcement Learning time
+        run(actor, critic, power_usage, 1000, myCar, seq_length, peak_consumption)
+
 
         i = 0
         while myCar.soc < myCar.max_battery_size and charge_rate != 0:
